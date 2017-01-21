680e29e4275243090ae3de14b0a853b9fe220238
Rafael Forbeck <rafaelforbeck@gmail.com>
2017-01-20 22:36:54 -0200

    Algumas importações iniciais

---
+++ Assets/Scripts/Input/Joystick.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Common/Input")]
+public class Joystick : ScriptableObject {
+
+    [Header("Axis")]
+    [SerializeField] string horizontal;
+    [SerializeField] string vertical;
+
+    [Header("Buttons")]
+    [SerializeField] string aButton;
+    [SerializeField] string bButton;
+    [SerializeField] string xButton;
+    [SerializeField] string yButton;
+
+    [SerializeField] string startButton;
+
+
+    public string AButton { get { return aButton; } }
+    public string BButton { get { return bButton; } }
+    public string XButton { get { return xButton; } }
+    public string YButton { get { return yButton; } }
+    public string StartButton { get { return startButton; } }
+    public string Horizontal { get { return horizontal; } }
+    public string Vertical { get { return vertical; } }
+
+}
---
+++ Assets/Scripts/Interface/LoadSceneOnClick.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour {
+
+	public void LoadByIndex(int sceneIndex)
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
