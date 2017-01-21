6465c1f8dd7ab42cc01578e676c76528190c2c38
Rafael Forbeck <rafaelforbeck@gmail.com>
2017-01-21 05:12:46 -0200

    Continuação da tela de seleção de personagens

--- Assets/CharacterMenu.cs
+++ Assets/CharacterMenu.cs
@@ -8,7 +8,8 @@ public class CharacterMenu : MonoBehaviour {
     int charactersCount;
     int currentIndexCharacter = 0;
     Image characterImage;
-
+    Image confirmImage;
+    bool selected = false;
 
     void Awake()
     {
@@ -18,26 +19,47 @@ public class CharacterMenu : MonoBehaviour {
     void Start()
     {
         charactersCount = CharactersManager.Instance.charactersSprites.Count;
-        characterImage = GetComponentInChildren<Image>();
+        characterImage = GetComponentsInChildren<Image>()[0];
+        confirmImage = GetComponentsInChildren<Image>()[1];
+        confirmImage.sprite = CharactersManager.Instance.confirmSprite;
+        confirmImage.enabled = false;
         characterImage.sprite = CharactersManager.Instance.pressStartSprite;
     }
 
     void Update()
     {
-        if (input.Right)
+        if (input.Right && !selected)
         {
             currentIndexCharacter = (currentIndexCharacter + 1) % charactersCount;
             characterImage.sprite = CharactersManager.Instance.GetCharacterSpriteByIndex(currentIndexCharacter);
         }
-        else if (input.Left)
+        else if (input.Left && !selected)
         {
             currentIndexCharacter = (charactersCount + currentIndexCharacter - 1) % charactersCount;
             characterImage.sprite = CharactersManager.Instance.GetCharacterSpriteByIndex(currentIndexCharacter);
         }
 
-        if (input.Confirm)
+        if (input.Cancel)
         {
-            
+            if (selected)
+            {
+                confirmImage.enabled = false;
+                selected = false;
+                CharactersManager.Instance.decreasePlayerReady();
+            }
+            else
+            {
+                characterImage.sprite = CharactersManager.Instance.pressStartSprite;
+                CharactersManager.Instance.ReturningPlayer(input.joysticks[0], gameObject);
+                input.enabled = false;
+                enabled = false;
+            }
+        }
+        else if (input.Confirm)
+        {
+            confirmImage.enabled = true;
+            selected = true;
+            CharactersManager.Instance.increasePlayerReady();
         }
     }
 
--- Assets/Scripts/Interface/CharactersManager.cs
+++ Assets/Scripts/Interface/CharactersManager.cs
@@ -2,17 +2,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class CharactersManager : MonoBehaviour {
 
     [Header("Imagens")]
     public List<Sprite> charactersSprites = new List<Sprite>();
     public Sprite pressStartSprite;
+    public Sprite confirmSprite;
+
     [Header("Canvas")]
     public List<GameObject> playerCanvas = new List<GameObject>();
 
+    [Header("Scene")]
+    public int nextSceneIndex = 2;
+
     List<Character> characters = new List<Character>();
     List<Joystick> joysticks = new List<Joystick>();
+
+    int playersCount;
+    int playersReady;
 
     static CharactersManager instance;
     public static CharactersManager Instance
@@ -38,23 +47,69 @@ public class CharactersManager : MonoBehaviour {
         {
             canvas.GetComponent<MenuInput>().enabled = false;
         }
-	}
+    }
 	
-	void Update () {
+	void Update ()
+    {
+        CheckJoinningPlayer();
+    }
+
+    private void CheckJoinningPlayer()
+    {
+        List<Joystick> removeJoysticks = new List<Joystick>();
         foreach (var joystick in joysticks)
         {
             if (Input.GetButtonDown(joystick.StartButton))
             {
                 playerCanvas[0].GetComponent<MenuInput>().enabled = true;
                 playerCanvas[0].GetComponent<MenuInput>().joysticks[0] = joystick;
+                playerCanvas[0].GetComponent<CharacterMenu>().enabled = true;
                 playerCanvas[0].GetComponent<CharacterMenu>().SetFirstCharacter();
                 playerCanvas.RemoveAt(0);
+                removeJoysticks.Add(joystick);
+                increasePlayerCount();
             }
         }
-	}
+        foreach (var joystick in removeJoysticks)
+        {
+            joysticks.Remove(joystick);
+        }
+    }
 
     public Sprite GetCharacterSpriteByIndex(int index)
     {
         return charactersSprites[index];
+    }
+    public void ReturningPlayer(Joystick joystick, GameObject canvas)
+    {
+        joysticks.Add(joystick);
+        playerCanvas.Insert(0, canvas);
+        decreasePlayerCount();
+    }
+
+    void increasePlayerCount()
+    {
+        playersCount++;
+    }
+    void decreasePlayerCount()
+    {
+        playersCount--;
+    }
+    public void increasePlayerReady()
+    {
+        playersReady++;
+        if (playersReady == playersCount)
+        {
+            Invoke("CallNextScene", 1);
+        }
+    }
+    public void decreasePlayerReady()
+    {
+        playersReady--;
+    }
+
+    void CallNextScene()
+    {
+        SceneManager.LoadScene(nextSceneIndex);
     }
 }
