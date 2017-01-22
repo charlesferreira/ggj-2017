bded9a4d6d2cc4222e46ffc3e387839b4fbfe2c8
Rafael Forbeck <rafaelforbeck@gmail.com>
2017-01-22 01:50:11 -0200

    Efeitos das ondas no cenário

---
+++ Assets/Scripts/Scenery/MiniWaves.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MiniWaves : MonoBehaviour {
+
+    public float speed = 1;
+    MeshRenderer mr;
+    public float phase;
+    public float amplitude = 10;
+
+	// Use this for initialization
+	void Start () {
+        mr = GetComponent<MeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        mr.sharedMaterial.mainTextureOffset += Vector2.left * speed * Time.deltaTime;
+        phase += Time.deltaTime;
+        transform.position += Vector3.up * amplitude * Mathf.Sin(phase);
+	}
+}
